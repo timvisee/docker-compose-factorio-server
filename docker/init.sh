5acce7595b79938e8b5ea171a2d946d580371eef
timvisee <timvisee@gmail.com>
2017-05-11 23:00:58 +0200

    Fix missing glibc in Alpine Linux causing Factorio to fail starting

--- docker/init.sh
+++ docker/init.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
 
+# Define some constants
+MOUNT_DIR=/factorio
+CONFIG_DIR=$MOUNT_DIR/config
+SAVES_DIR=$MOUNT_DIR/saves
+MODS_DIR=$MOUNT_DIR/mods
+SERVER_SETTINGS_FILE=$CONFIG_DIR/server-settings.json
+MAP_GEN_SETTINGS_FILE=$CONFIG_DIR/map-gen-settings.json
+MAP_SETTINGS_FILE=$CONFIG_DIR/map-settings.json
+MAP_FILE=$SAVES_DIR/map.zip
+
 echo "Initializing Factorio server..."
 
-echo "#### $ chmod a+x [file]:"
-chmod a+x /opt/factorio/bin/x64/factorio
-echo "#### $ stat [file]:"
-stat /opt/factorio/bin/x64/factorio
-echo "#### $ ls -al [file]:"
-ls -al /opt/factorio/bin/x64/factorio
-echo "#### $ uname -a:"
-uname -a
-echo "#### $ [file]:"
-/opt/factorio/bin/x64/factorio
-echo "#### $ bash -c [file]:"
-bash -c /opt/factorio/bin/x64/factorio
-echo "#### $ eval [file]:"
-eval /opt/factorio/bin/x64/factorio
+# Make sure the mount directory is mounted
+if [ ! -d "$MOUNT_DIR" ]; then
+    echo "Factorio directory not mounted, quitting."
+    echo "Please mount to the '/factorio' directory to start the server."
+    exit 1
+fi
+
+# Make sure the desired directories and configurations are available
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Creating config directory..."
+    mkdir "$CONFIG_DIR"
+fi
+if [ ! -f "$SERVER_SETTINGS_FILE" ]; then
+    echo "Creating server settings file..."
+    cp /opt/factorio/data/server-settings.example.json $SERVER_SETTINGS_FILE
+fi
+if [ ! -f "$MAP_GEN_SETTINGS_FILE" ]; then
+    echo "Creating map generator settings file..."
+    cp /opt/factorio/data/map-gen-settings.example.json $MAP_GEN_SETTINGS_FILE
+fi
+if [ ! -f "$MAP_SETTINGS_FILE" ]; then
+    echo "Creating map settings file..."
+    cp /opt/factorio/data/map-settings.example.json $MAP_SETTINGS_FILE
+fi
+if [ ! -d "$SAVES_DIR" ]; then
+    echo "Creating saves directory..."
+    mkdir "$SAVES_DIR"
+fi
+if [ ! -d "$MODS_DIR" ]; then
+    echo "Creating mods directory..."
+    mkdir "$MODS_DIR"
+fi
+
+echo "Changing into Factorio directory..."
+cd /opt/factorio/bin/x64
+
+# Generate a map if it doesn't exist
+if [ ! -f "$MAP_FILE" ]; then
+    echo "Map doesn't exist, generating..."
+    
+    # Generate the map
+    ./factorio --create $MAP_FILE --server-settings $SERVER_SETTINGS_FILE --map-gen-settings $MAP_GEN_SETTINGS_FILE --map-settings $MAP_SETTINGS_FILE
+fi
+
+echo "Starting Factorio server..."
+
+# Start the server, with the latest available save
+exec ./factorio --start-server-load-latest --server-settings $SERVER_SETTINGS_FILE --map-gen-settings $MAP_GEN_SETTINGS_FILE --map-settings $MAP_SETTINGS_FILE
+
+echo "Factorio server stopped!"
